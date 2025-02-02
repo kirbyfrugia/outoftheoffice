455412ed2a468b54477d4becbad75855b80cbf46
Kirby Frugia <kirby.frugia@gmail.com>
2025-02-02 06:46:12 -0800

    blah

--- tiles.asm
+++ tiles.asm
@@ -1,15 +1,24 @@
-.var tiles_ul = $2000
-.var tiles_ur = $2100
-.var tiles_bl = $2200
-.var tiles_br = $2300
-.var level_tiles = $2400
+// .var tiles_ul = $2000
+// .var tiles_ur = $2100
+// .var tiles_bl = $2200
+// .var tiles_br = $2300
+// .var level_tiles = $2400
 
-gen_tiles:
-  ldx #0
-  ldy #0
-test_tiles_loop:
-  
-  iny
-  beq test_tiles_loop_done
-test_tiles_loop_done:
+.macro draw_tile_row(scr_row_upper, scr_row_lower, map_data_tile_row) {
+  ldx #20
+draw_tile_row_loop:
+
+  dex
+  bne draw_tile_row_loop
+}
+
+draw_screen:
+  lda #<map_data
+  sta zpb0
+  lda #>map_data
+  sta zpb0
+
+  ldx #3
+draw_screen_row_loop:
+  draw_tile_row(1144, 1184, map_data+(MAP_WID*3))
   rts
